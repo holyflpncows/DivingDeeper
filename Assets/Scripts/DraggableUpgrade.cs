ab413c4dec27af3cbd6f3669364ef72410c55f11
Tylar.Jensen <Tylar.Jensen@strongmind.com>
2023-08-22 21:34:19 -0700

    draggables!! but we need to mostly get rid of it.

--- Assets/Scripts/DraggableUpgrade.cs
+++ Assets/Scripts/DraggableUpgrade.cs
@@ -4,30 +4,46 @@ using Upgrades;
 
 public class DraggableUpgrade : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    private CanvasGroup _canvasGroup;
+    private Vector3 _originalAnchoredPosition;
+
     public Part Upgrade;
+    public Canvas Canvas;
+    [HideInInspector]
+    public Transform ParentAfterDrag;
+    [HideInInspector]
+    public bool DroppedInSlot = false;
 
-    void Start()
+    private void Awake()
     {
-
-    }
-
-    void Update()
-    {
-
+        _canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("OnBeginDrag");
+        ParentAfterDrag = transform.parent;
+        _originalAnchoredPosition = transform.position;
+
+        _canvasGroup.blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        Debug.Log("OnDrag");
+        transform.position = Input.mousePosition;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        Debug.Log("OnEndDrag");
+        if (DroppedInSlot)
+        {
+            transform.SetParent(ParentAfterDrag, false);
+            transform.position = ParentAfterDrag.position;
+        }
+        else
+        {
+            transform.position = _originalAnchoredPosition;
+        }
+
+        _canvasGroup.blocksRaycasts = true;
     }
 }
---
+++ Assets/Scripts/UpgradeSlot.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UpgradeSlot : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        var dropped = eventData.pointerDrag;
+        var draggable = dropped.GetComponent<DraggableUpgrade>();
+        draggable.ParentAfterDrag = transform;
+        draggable.DroppedInSlot = true;
+    }
+}
