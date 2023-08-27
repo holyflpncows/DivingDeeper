53810fb37924369616b9ae12cf0025c260053461
James Osborn <3580335+JamesOsborn-SE@users.noreply.github.com>
2023-08-26 21:00:07 -0700

    draggish

--- Assets/Scripts/SubmarineMovement.cs
+++ Assets/Scripts/SubmarineMovement.cs
@@ -60,8 +60,8 @@ public class SubmarineMovement : MonoBehaviour
     private void Move()
     {
         rb.velocity = new Vector2(
-            _moveDirection.x * moveSpeed*(1/(Submarine.Instance.GetDrag+1)),
-            _moveDirection.y * moveSpeed*(1/(Submarine.Instance.GetDrag+1)));
+            _moveDirection.x * moveSpeed*(Submarine.Instance.GetDrag/10),
+            _moveDirection.y * moveSpeed*(Submarine.Instance.GetDrag/10));
         _facingRight = _moveDirection.x > 0 && _moveDirection.x != 0;
     }
     
